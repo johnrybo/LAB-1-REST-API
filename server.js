bab9528990c00d521188c3867f6b94155c57ca23
John <john.rybo@gmail.com>
2021-04-08 09:43:14 +0200

    starter files

--- server.js
+++ server.js
@@ -2,13 +2,42 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res, next) => {
-    // nånting
-    next();
+const products = [
+    {
+        "name": "Stol",
+        "description": "En fin stol",
+        "price": 1499
+    },
+    {
+        "name": "Soffa",
+        "description": "En skön soffa",
+        "price": 8999
+    }
+];
+
+// Serve all files in the public folder
+app.use(express.static('public'))
+
+// Parse incoming body from json to js-object
+app.use(express.json());
+
+// Define our endpoints
+app.get('/api/products', (req, res) => {
+    res.json(products);
 })
 
-app.use(express.static('public'));
+app.post('/api/products', (req, res) => {
+    products.push(req.body);
+    res.status(201).json(req.body);
+})
 
+app.delete('/api/products', (req, res) => {
+    const index = products.findIndex(p => p.name == "Sebastians tröja");
+    const deletedProduct = products.splice(index, 1);
+    res.json(deletedProduct);
+})
+
+// Start the server
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`)
+    console.log(`Server is running on http://localhost:${port}`)
 })
