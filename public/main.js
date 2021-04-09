5671fea19d4cc8bff80b32b5f2be13b10461b4f9
John <john.rybo@gmail.com>
2021-04-09 16:22:05 +0200

    fix

--- public/main.js
+++ public/main.js
@@ -1,42 +1,41 @@
-window.addEventListener("load", initSite)
+window.addEventListener("load", initSite);
 
- async function initSite() {
-    const allProducts = await getAllProducts()
-    console.log(allProducts)
-    // getSpecificProduct(3)
-    // createNewProduct("Köra bil")
+async function initSite() {
+  const allProducts = await getAllProducts();
+  console.log(allProducts);
+  const specificProduct = await getSpecificProduct(2);
+  console.log(specificProduct);
+  const newProduct = await createNewProduct("Bord", "Ett bra bord", 999);
+  console.log(newProduct);
 }
 
 async function getAllProducts() {
-    const products = await makeRequest("/api", "GET")
-    return products
+  const products = await makeRequest("/api", "GET");
+  return products;
 }
 
 async function getSpecificProduct(id) {
-    const product = await makeRequest("/api/" + id, "GET")
-    console.log(product)
+  const product = await makeRequest("/api/products/" + id, "GET");
+  return product;
 }
 
-async function createNewProduct(title) {
+async function createNewProduct(name, description, price) {
+  const body = { name: name, description: description, price: price };
 
-    const body = {title: title}
-
-    const status = await makeRequest("/api", "POST", body)
-    console.log(status)
+  const status = await makeRequest("/api", "POST", body);
+  return status;
 }
 
 async function makeRequest(url, method, body) {
+  const response = await fetch(url, {
+    method: method,
+    body: JSON.stringify(body),
+    headers: {
+      "Content-type": "application/json",
+    },
+  });
 
-    const response = await fetch(url, {
-        method: method,
-        body: JSON.stringify(body),
-        headers: {
-            'Content-type': 'application/json'
-        }
-    })
-
-    console.log(response)
-    const result = await response.json()
-    return result
-
+  console.log(response);
+  const result = await response.json();
+  return result;
 }
